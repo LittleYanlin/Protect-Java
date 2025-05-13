daef361f9a59536e78a813948b39a73350c48668
LittleYanlin <zhouyanlin666@outlook.com>
2025-05-13 23:59:35 +0800

    重构switchCard方法，优化面板切换逻辑，确保面板正确添加和移除

--- src/Game/App.java
+++ src/Game/App.java
@@ -23,7 +23,26 @@ public class App extends JFrame implements CardSwitcher {
         cardLayout.show(cardPanel, "START");
     }
     public void switchCard(String cardName) {//设置回调函数
-        cardLayout.show(cardPanel, cardName);
+        switch(cardName) {
+            case "LOADING":
+                cardPanel.add(loadingPanel, "LOADING");
+                break;
+            case "GAME1":
+                cardPanel.add(new Mission1(this), "GAME1");
+                break;
+            case "GAME2":
+                cardPanel.add(new Mission2(this), "GAME2");
+                break;
+            case "GAME3":
+                cardPanel.add(new Mission3(this), "GAME3");
+                break;
+            case "START":
+                cardPanel.add(new StartPanel(this), "START");
+                break;
+        }
+        cardLayout.show(cardPanel, cardName);//显示回调函数要求的面板
+        cardPanel.remove(0);//移除上一个面板
+        cardPanel.revalidate();//刷新面板
     }
     public static void main(String[] args) {
         App app = new App();
